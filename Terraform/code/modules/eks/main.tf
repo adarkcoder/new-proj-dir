92d7bcf6024bce000b547be94c7c385aebe66814
MANOJ <manojmat2000@gmail.com>
2024-07-06 20:17:16 +0530

    jenkins

--- Terraform/code/modules/eks/main.tf
+++ Terraform/code/modules/eks/main.tf
@@ -26,12 +26,12 @@ resource "aws_eks_node_group" "eks_node_group" {
   subnet_ids      = var.private_subnets
 
   scaling_config {
-    desired_size = 3
+    desired_size = 2
     max_size     = 3
-    min_size     = 3
+    min_size     = 2
   }
 
-  instance_types = ["t3.medium"]
+  instance_types = ["t3.large"]
 
   remote_access {
     ec2_ssh_key = aws_key_pair.my_key.key_name
